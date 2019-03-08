24377340ecde2c0fc1ebe74853beb7b85d63c5b1
ishakmehta04 <ishakmehta04@gmail.com>
2019-03-08 15:05:38 -0500

    Refactoring the shuffle function and using es6

--- cardgame/src/app/app.component.ts
+++ cardgame/src/app/app.component.ts
@@ -27,89 +27,51 @@ export class AppComponent {
       color: 'black',
       glyph: '&#x2660;'
     }];
-    listOfCards = [];
+  listOfCards = [];
 	selectedCard = [];
 	
 	
-	randomCardPicker(min:number, max:number) {
-          return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-	
 	formatData(cardType, letter:string, name:string) {
-      return {
-        cardType,
-        letter,
-        name: name || letter,
-        displayName: (name || letter) + ' of ' + cardType.name
-      };
-    }
-	
-	divide(listOfCards) {
-        if(listOfCards.length > 1) {
-          let middle = Math.floor(listOfCards.length / 2);
-          let variance = this.randomCardPicker(0, 12) - 6;
-          middle += variance;
-          middle = Math.max(middle, 1);
-          return {
-            top: listOfCards.slice(0, middle),
-            bottom: listOfCards.slice(middle)
-          };
-        }
-
-        return {
-          top: (listOfCards.length === 1) ? [listOfCards[0]] : [],
-          bottom: []
-        };
+    return {
+      cardType,
+      letter,
+      name: name || letter,
+      displayName: (name || letter) + ' of ' + cardType.name
+    };
     }
 	
 	shuffle() {
-        // repeat 20 times for a new deck
-        for (let i = 0; i < 20; i++) {
-          // cut the cards in half
-          let split = this.divide(this.listOfCards);
-          // we will stack both halves into this pile
-          let deck = [];
-          while (split.top.length > 0 || split.bottom.length > 0) {
-            // a random number of cards to take from the top
-            let take = this.randomCardPicker(1, 5);
-            // take that many cards from the top and put in the pile
-            deck = deck.concat(split.top.splice(0, take));
-            // a random number of cards to take from the bottom
-            take = this.randomCardPicker(1, 5);
-            // take that many cards from the bottom and put in the pile
-            deck = deck.concat(split.bottom.splice(0, take));
-          }
-          // put the bottom onto the top so cards are mixed up more
-          let temp = this.divide(deck);
-          this.listOfCards = temp.bottom.concat(temp.top);
+    for(let i = this.listOfCards.length - 1; i > 0; i--) {
+      let randomIndex = Math.floor(Math.random() * i);
+      [this.listOfCards[i], this.listOfCards[randomIndex]] = [this.listOfCards[randomIndex], this.listOfCards[i]];
+    }
+  }
+	  
+  dealOne() {
+    // get a card from the deck
+    let nextCard = this.listOfCards.shift();
+    if (nextCard) {
+      this.selectedCard.push(nextCard);
+      this.selectedCard = this.selectedCard.slice();
+    }
+  }
+	  
+  reset() {
+    this.listOfCards = [];
+    this.selectedCard = [];
+    let self = this;
+    this.availableCards.forEach(function(value) {
+        self.listOfCards.push(self.formatData(value, 'A', 'Ace'));
+        for (let i = 2; i <= 10; i++) {
+          self.listOfCards.push(self.formatData(value, i+'',''));
         }
-      }
-	  
-	  dealOne() {
-      // get a card from the deck
-      let nextCard = this.listOfCards.shift();
-      if (nextCard) {
-        this.selectedCard.push(nextCard);
-		    this.selectedCard = this.selectedCard.slice();
-      }
+        self.listOfCards.push(self.formatData(value, 'J', 'Jack'));
+        self.listOfCards.push(self.formatData(value, 'Q', 'Queen'));
+        self.listOfCards.push(self.formatData(value, 'K', 'King'));
+      });
     }
-	  
-	  reset() {
-      this.listOfCards = [];
-		  this.selectedCard = [];
-		  let self = this;
-      this.availableCards.forEach(function(value) {
-          self.listOfCards.push(self.formatData(value, 'A', 'Ace'));
-          for (let i = 2; i <= 10; i++) {
-            self.listOfCards.push(self.formatData(value, i+'',''));
-          }
-          self.listOfCards.push(self.formatData(value, 'J', 'Jack'));
-          self.listOfCards.push(self.formatData(value, 'Q', 'Queen'));
-          self.listOfCards.push(self.formatData(value, 'K', 'King'));
-        });
-      }
-	  
-	  ngOnInit() {
-        this.reset();
-    }
+  
+  ngOnInit() {
+      this.reset();
+  }
 }
